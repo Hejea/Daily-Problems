5891cdda029758b4dbc524d506fe15317f7a0e87
Hejea <hejea@mail.ru>
2018-06-23 22:14:27 +0300

    Adding Program-2

---
+++ Problem-2/src/hejea/Main.java
@@ -0,0 +1,15 @@
+package hejea;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+
+        solution.showSolution(list);
+    }
+}
---
+++ Problem-2/src/hejea/Solution.java
@@ -0,0 +1,37 @@
+package hejea;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Solution {
+    
+    public static void showArray (int[] array) {
+        System.out.println("\nVector :");
+        for (Integer a : array) {
+            System.out.print(a + " ");
+        }
+        System.out.println();
+    }
+    
+    public void showSolution (List<Integer> array) {
+        
+        int i, j, n;
+        n = array.size();
+        
+        int[] a1 = new int[n];
+        int[] a2 = new int[n];
+        
+        a1[0] = a2[0] = 1;
+        a1[1] = array.get(0);
+        a2[1] = array.get(n -1);
+        
+        for (i = 2, j = n - 2; i < n; i++, j--) {
+            a1[i] = a1[i - 1] * array.get(i-1);
+            a2[i] = a2[i - 1] * array.get(j);
+        }
+        
+        for (i = 0; i < n; i++) a1[i] = a1[i] * a2[n - 1 - i];
+        
+        showArray(a1);
+    }
+}
